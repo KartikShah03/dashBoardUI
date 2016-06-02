7511a2b15eb419bae93f75cb3bb7350b43488325
Suresh Dusa <dusa.suresh@tcs.com>
2016-06-02 13:51:57 +0800

    Header name changes

--- src/app/pages/login/LoginPageCtrl.js
+++ src/app/pages/login/LoginPageCtrl.js
@@ -17,7 +17,7 @@
       if("Dashboard" == $scope.pwd){
         console.log($scope.pwd)
         console.log('before');
-        $http.get("http://demo5533116.mockable.io/sidemenu")
+        $http.get("http://demo0495135.mockable.io/menuitems")
          .then(function(response){
           baSidebarService.setMenuList(response.data);
           $scope.$parent.loginDiv = false;
