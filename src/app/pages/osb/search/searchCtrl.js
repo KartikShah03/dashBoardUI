e416a69dda92bb3e510840a7a6463ca23b4ba1af
KartikShah03 <kartik3.s@tcs.com>
2016-06-17 11:30:56 +0800

    camel casing changes.

--- src/app/pages/osb/search/searchCtrl.js
+++ src/app/pages/osb/search/searchCtrl.js
@@ -8,38 +8,9 @@
   function searchCtrl($scope, $window, baConfig,serviceCall) {
     var layoutColors = baConfig.colors;
     $scope.colors = [layoutColors.primary, layoutColors.warning, layoutColors.danger, layoutColors.info, layoutColors.success, layoutColors.primaryDark];
-    $scope.lineData = [
-      {y: "2006", a: 100, b: 90},
-      {y: "2007", a: 75, b: 65},
-      {y: "2008", a: 50, b: 40},
-      {y: "2009", a: 75, b: 65},
-      {y: "2010", a: 50, b: 40},
-      {y: "2011", a: 75, b: 65},
-      {y: "2012", a: 100, b: 90}
-    ];
-    $scope.areaData = [
-      {y: "2006", a: 100, b: 90},
-      {y: "2007", a: 75, b: 65},
-      {y: "2008", a: 50, b: 40},
-      {y: "2009", a: 75, b: 65},
-      {y: "2010", a: 50, b: 40},
-      {y: "2011", a: 75, b: 65},
-      {y: "2012", a: 100, b: 90}
-    ];
-    $scope.barData = [
-      {y: "2006", a: 100, b: 90},
-      {y: "2007", a: 75, b: 65},
-      {y: "2008", a: 50, b: 40},
-      {y: "2009", a: 75, b: 65},
-      {y: "2010", a: 50, b: 40},
-      {y: "2011", a: 75, b: 65},
-      {y: "2012", a: 100, b: 90}
-    ];
-    $scope.donutData = [
-      {label: "Download Sales", value: 12},
-      {label: "In-Store Sales", value: 30},
-      {label: "Mail-Order Sales", value: 20}
-    ];
+    var date = new Date();
+    $scope.endDate = date;
+    //$scope.sources = "test source";
     $scope.doShow = false;
 
     function convertDate(inputFormat) {
